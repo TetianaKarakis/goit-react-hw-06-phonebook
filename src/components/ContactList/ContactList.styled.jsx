5d5e1ac68b85dd513200d119151336888cfabf28
TetianaKarakis <tetiana.karakis@gmail.com>
2023-09-23 19:59:39 +0300

    .

--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -21,7 +21,7 @@ export const ListItem = styled.li`
     margin-top: 15px;
     margin-bottom: 15px;
     font-family: 'Open Sans';
-    color: #010711;
+    color: #010713;
     font-size: 20px;
     font-weight: bold;
     gap: 8px;
